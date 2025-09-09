c9f49e94312e07e67230c5e1a8ed3ce477af6c34
viptalya <vip_10@bk.ru>
2025-09-09 09:08:55 +0300

    добавил функцию возведения в степень

--- funcPractic.go
+++ funcPractic.go
@@ -8,19 +8,26 @@ import (
 func main() {
 	a := Sum(1, 4)
 	b := Subtraction(2, 6)
-	c, err := Division(5.0, 0.0)
+	c, err1 := Division(5.0, 0.0)
 	d := Multiplication(2, 7)
+	e, err2 := Degree(5, 0)
 
 	fmt.Println(a)
 	fmt.Println(b)
 
-	if err != nil {
-		fmt.Println(err)
+	if err1 != nil {
+		fmt.Println(err1)
 	} else {
 		fmt.Println(c)
 	}
 
 	fmt.Println(d)
+
+	if err2 != nil {
+		fmt.Println(err2)
+	} else {
+		fmt.Println(e)
+	}
 }
 
 // Функция сложения
@@ -49,4 +56,23 @@ func Division(a, b float64) (float64, error) {
 // Функция умножения
 func Multiplication(a, b int) int {
 	return a * b
+}
+
+// Функция возведения в указанную степень
+func Degree(n float64, e int) (float64, error) {
+	if n == 0.0 {
+		return 0.0, errors.New("значение не опредено")
+	}
+
+	if e == 0 {
+		return 1.0, nil
+	}
+
+	result := 1.0
+
+	for i := 1; i <= e; i++ {
+		result *= n
+	}
+
+	return result, nil
 }
